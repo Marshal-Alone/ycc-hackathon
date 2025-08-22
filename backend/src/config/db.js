f5a1c1cc135293f718b3c20d6c7aa03db986b962
Marshal <marshal3869@gmail.com>
2025-08-22 23:05:47 +0530

    Mongodb atlas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      ssl: true,
+      tls: true,
+    });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
