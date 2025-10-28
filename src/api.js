b483295e7571fcb501275ce36d0d9d7ee73aabf9
Marshal Alone <145204636+Marshal-Alone@users.noreply.github.com>
2025-10-28 23:36:49 +0530

    Backend url fix

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
+console.log("Backend on : ",API)
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
